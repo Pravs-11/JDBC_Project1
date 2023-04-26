657a814f83ebc943d62e817914ca6335e1d15e6f
Pravs_11 <thotapalli9866@gmail.com>
2023-04-26 22:43:11 +0530

    testing done

--- src/test/java/com/stockMarket/AppTest.java
+++ src/test/java/com/stockMarket/AppTest.java
@@ -1,38 +1,68 @@
 package com.stockMarket;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+public class AppTest{
+	
+	Stockdao b1=new Stockdao();
+	@Disabled
+	@Test
+	public void testregister() throws Exception {
+		Details c1=new Details();
+		c1.cusid=5;
+		c1.cusname="pravallika";
+		c1.cuspin=2020;
+		c1.cusStocks=50;
+		b1.dbconnection();
+		int exp=1;
+		int res=b1.registercustomer(c1);
+		assertEquals(exp,res);
+	}
+	
+	
+	@Disabled
+	@Test
+	public void testlogin() throws Exception {
+		b1.dbconnection();
+		int exp=1;
+		int res=b1.login("xyz", 123);
+		assertEquals(exp,res);
+	}
+	@Disabled
+	@Test
+	public void testdeposit() throws Exception {
+		b1.dbconnection();
+		int exp=40010;
+		int res=b1.deposit(40010, 1);
+		assertEquals(exp,res);
+	}
+	@Disabled
+	@Test
+	public void testwithdraw() throws Exception {
+		b1.dbconnection();
+		int exp=5;
+		int res=b1.withdraw(10,1235,12);
+		assertEquals(exp,res);
+	}
+	//test case to test changepassword method
+	@Disabled
+	@Test
+	public void testchangepwd() throws Exception {
+		b1.dbconnection();
+		int exp=1;
+		int res=b1.changepin(123,321,1);
+		assertEquals(exp,res);
+	}
+	//test case to test delete account method
+	
+	@Test
+	public void testdelaccount() throws Exception {
+		b1.dbconnection();
+		int exp=1;
+		int res=b1.deleteAccount(321,1);
+		assertEquals(exp,res);
+	}
 }
